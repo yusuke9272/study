0abbd7ea65cabf169f198ca22ab817dd5d738e43
yusuke9272 <160245634+yusuke9272@users.noreply.github.com>
2024-08-09 17:40:50 +0900

    Add files via upload

---
+++ lesson0802/main.js
@@ -0,0 +1,16 @@
+const simpleAcc = (wrapper) => {
+    const menu = wrapper.firstElementChild;
+    const contents = wrapper.lastElementChild;
+
+    const onClick = () => {
+        const isOpen = wrapper.classList.contains('open');
+
+        contents.style.height = `${isOpen ? '0' : contents.scrollHeight}px`;
+        wrapper.classList.toggle('open');        
+    }
+
+    menu.addEventListener('click',onClick);
+}
+
+const accs = document.querySelectorAll('[data-acc]');
+accs.forEach(simpleAcc);
